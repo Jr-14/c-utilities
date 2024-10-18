0d8b91d22763e9bb61acddb372bf303ee59f799a
John Emerson Requizo <jr.jemerson14@gmail.com>
2024-10-18 21:22:05 +1100

    Add dynamic array for ints

--- src/lib/dynamicArray.c
+++ src/lib/dynamicArray.c
@@ -8,7 +8,15 @@ void grow(DynamicArray *a) {
 void shrink(DynamicArray *a) {
 }
 
+void freeArray(DynamicArray *a) {
+  free(a->array);
+  a->array = NULL;
+  a->capacity = 0;
+  a->size = 0;
+}
+
 void insert(DynamicArray *a, void *element) {
+  printf("inserting\n");
   if (a->size == a->capacity) {
     grow(a);
   }
@@ -17,9 +25,43 @@ void insert(DynamicArray *a, void *element) {
 }
 
 void initArray(DynamicArray *a, size_t initialSize, size_t elementSize) {
+  printf("creating array\n");
   a->array = calloc(initialSize, elementSize);
   a->capacity = 0;
   a->size = initialSize;
   a->elementSize = elementSize;
+  printf("finished creating array\n");
 }
 
+DynamicArrayInt initArrayInt(size_t initialCapacity) {
+  DynamicArrayInt a;
+  printf("Creating array\n");
+  a.array = calloc(initialCapacity, sizeof(int));
+  a.capacity = initialCapacity;
+  a.size = 0;
+  printf("finished creating array");
+  return a;
+}
+
+void growArrayInt(DynamicArrayInt *a) {
+  a->capacity *= GROW_CAPACITY;
+  a->array = realloc(a->array, sizeof(int) * a->capacity);
+}
+
+void insertIntIntoArray(DynamicArrayInt *a, int element) {
+  if (a->size == a->capacity) {
+    growArrayInt(a);
+  }
+
+  memcpy((char*)a->array + (a->size * sizeof(int)), &element, sizeof(int));
+  a->size++;
+
+  printf("Array size is %d, and array capacity is %d\n", (int)a->size, (int)a->capacity);
+}
+
+void printDynamicArrayInt(DynamicArrayInt *a) {
+  for (size_t i = 0; i < a->size; i++) {
+      int *integer = (int*)((char*)a->array + i * sizeof(int));
+      printf("integer is %d\n", *integer);
+  }
+}
--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include "lib/dynamicArray.h"
 
+void printArray(DynamicArray *a) {
+  for (size_t i = 0; i < a->size; i++) {
+    printf("%lu ", *(int*)(char*)a->array + i * a->elementSize);
+  }
+  printf("\n");
+}
+
 int main() {
   printf("hello, world!!\n");
-  
-  DynamicArray a;
-  initArray(&a, 3, sizeof(int));
-  insert(&a, (void*)5);
+
+  DynamicArrayInt a = initArrayInt(1);
+  insertIntIntoArray(&a, 4);
+  insertIntIntoArray(&a, 3);
+  insertIntIntoArray(&a, 2);
+  insertIntIntoArray(&a, 1);
+  insertIntIntoArray(&a, 0);
+  printDynamicArrayInt(&a);
+  // DynamicArray a;
+  // initArray(&a, 4, sizeof(int));
+  // printf("about to insert stuff\n");
+  // insert(&a, (void*)5);
+  // insert(&a, (void*)6);
+  // printArray(&a);
+
 }
